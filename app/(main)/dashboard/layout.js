727a296f7da88b0f981dfb1ef4b7fd4df1e3906f
Madhur Pathak <117804227+Madhur1707@users.noreply.github.com>
2024-12-27 17:31:29 +0530

    New account form & schema

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,16 +1,16 @@
 import { Suspense } from "react";
 import DashboardPage from "./page";
-import { BarLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners";
 
 const DashboardLayout = () => {
   return (
     <div className="px-5">
-      <h1 className="text-4xl md:text-6xl lg:text-[65px] pb-6 bg-gradient-to-r from-black via-gray-800 to-purple-900 text-transparent bg-clip-text font-semibold tracking-tight leading-tight">
+     <h1 className="text-4xl md:text-6xl lg:text-[75px] pb-6 bg-gradient-to-r from-black via-gray-800 to-purple-900 text-transparent bg-clip-text font-semibold tracking-tight leading-tight ml-3">
         Dashboard
       </h1>
 
       <Suspense
-        fallback={<BarLoader className="mt-4" width={"100%"} color="#933ea" />}
+        fallback={<ClipLoader className="mt-4" width={"100%"} color="#933ea" />}
       >
         <DashboardPage />
       </Suspense>
