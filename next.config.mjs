1d515aad4f189c3798045c49c4e3beec73598529
Madhur Pathak <117804227+Madhur1707@users.noreply.github.com>
2024-12-28 16:51:28 +0530

    logo bug fix in prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
+    images: {
+        remotePatterns: [
             {
-                protocol:"https",
-                hostname:"randomuser.me"
+                protocol: "https",
+                hostname: "randomuser.me"
             },
             {
-                protocol:"https",
-                hostname:"https://my-vault-two.vercel.app/"
+                protocol: "https",
+                hostname: "my-vault-two.vercel.app"
             }
         ]
     }
