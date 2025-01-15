849db7de663256f742b8c133003eeebdf1cc0c9b
Madhur Pathak <117804227+Madhur1707@users.noreply.github.com>
2025-01-15 17:09:23 +0530

    budget progress card Ui and states

--- app/(main)/dashboard/_components/BudgetProgress.jsx
+++ app/(main)/dashboard/_components/BudgetProgress.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -5,17 +7,77 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { Check, Pencil, X } from "lucide-react";
+import React, { useState } from "react";
 
-const BudgetProgress = () => {
+const BudgetProgress = ({ initialBudget, currentExpenses }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newBudget, setNewBudget] = useState(
+    initialBudget?.amount?.toString() || ""
+  );
+
+  const percentUsed = initialBudget
+    ? (currentExpenses / initialBudget.amount) * 100
+    : 0;
+
+  const handleUpdateBudget = () => {};
+
+  const handleCancle = () => {
+    setNewBudget(initialBudget?.amount?.toString() || "");
+    setIsEditing(false);
+  };
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Monthly Budget (Default Account)</CardTitle>
-        <CardDescription></CardDescription>
+    <Card className="mb-5">
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <div className="flex-1">
+          <CardTitle>Monthly Budget (Default Account)</CardTitle>
+          <div className="flex items-center gap-2 mt-3">
+            {isEditing ? (
+              <div className="flex items-center gap-1">
+                <Input
+                  type="number"
+                  value={newBudget}
+                  onChange={(e) => setNewBudget(e.target.value)}
+                  className="w-32"
+                  placeholder="Enter Amount"
+                  autoFocus
+                />
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={handleUpdateBudget}
+                >
+                  <Check className="h-4 w-4 text-green-500" />
+                </Button>
+                <Button variant="ghost" size="icon" onClick={handleCancle}>
+                  <X className="w-4 h-4 text-red-500" />
+                </Button>
+              </div>
+            ) : (
+              <>
+                <CardDescription>
+                  {initialBudget
+                    ? `₹${currentExpenses.toFixed(
+                        2
+                      )} of ₹${initialBudget.amount.toFixed(2)} spent`
+                    : "No Budget Set"}
+                </CardDescription>
+
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => setIsEditing(true)}
+                  className="h-6 w-6"
+                >
+                  <Pencil className="h-3 w-3" />
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
       </CardHeader>
-      <CardContent></CardContent>
-      BudgetProgress
+      <CardContent> Budget Progress</CardContent>
     </Card>
   );
 };
