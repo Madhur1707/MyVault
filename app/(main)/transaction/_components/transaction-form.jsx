7c95bf63918753f93c17d5267f8eb8c86b125cc6
Madhur Pathak <117804227+Madhur1707@users.noreply.github.com>
2025-03-01 16:43:04 +0530

    create form and schema for add transaction

--- app/(main)/transaction/_components/transaction-form.jsx
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -1,12 +1,117 @@
 "use client";
+import { createTransaction } from "@/action/transaction";
+import { transactionSchema } from "@/app/lib/schema";
+import CreateAccountDrawer from "@/components/CreateAccountDrawer";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import useFetch from "@/hooks/use-fetch";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
+const AddTrasactionForm = ({ accounts, categories }) => {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    getValues,
+    reset,
+  } = useForm({
+    resolver: zodResolver(transactionSchema),
+    defaultValues: {
+      type: "EXPENSE",
+      amount: "",
+      description: "",
+      accountId: accounts.find((ac) => ac.isDefault)?.id,
+      date: new Date(),
+      isRecurring: false,
+    },
+  });
 
-const AddTrasactionForm = () => {
+  const {
+    loading: transactionLoading,
+    fn: transactionFn,
+    data: transactionResult,
+  } = useFetch(createTransaction);
+
+  const type = watch("type");
+  const isRecurring = watch("isRecurring");
+  const date = watch("date");
+
   return (
-    <div>
-      AddTrasactionForm
-    </div>
-  )
-}
+    <form className="space-y-6">
+      {/* AI Reciept Scanner  */}
+      <div className="space-y-2">
+        <label className="text-sm font-medium"> Type</label>
+        <Select
+          onValueChange={(value) => setValue("type", value)}
+          defaultValue={type}
+        >
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="EXPENSE">Expense</SelectItem>
+            <SelectItem value="INCOME">Income</SelectItem>
+          </SelectContent>
+        </Select>
 
-export default AddTrasactionForm
+        {errors.type && (
+          <p className="text-sm text-red-500"> {errors.type.message}</p>
+        )}
+      </div>
+
+      <div className="grid gap-6 md:grid-cols-2">
+        <div className="space-y-2">
+          <label className="text-sm font-medium">Amount</label>
+          <Input
+            type="number"
+            step="0.01"
+            placeholder="0.00"
+            {...register("amount")}
+          />
+          {errors.amount && (
+            <p className="text-sm text-red-500"> {errors.type.message}</p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">Account</label>
+          <Select
+            onValueChange={(value) => setValue("accountId", value)}
+            defaultValue={getValues("accountId")}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select account" />
+            </SelectTrigger>
+            <SelectContent>
+              {accounts.map((account) => (
+                <SelectItem key={account.id} value={account.id}>
+                  {account.name} (₹{parseFloat(account.balance).toFixed(2)})
+                </SelectItem>
+              ))}
+
+              <CreateAccountDrawer>
+                <Button variant="ghost" className="w-full select-none items-center text-sm outline-none">Create Account</Button>
+              </CreateAccountDrawer>
+            </SelectContent>
+          </Select>
+
+          {errors.type && (
+            <p className="text-sm text-red-500"> {errors.type.message}</p>
+          )}
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default AddTrasactionForm;
