91782d560f5d42fafc4f7b0bbd8df30f787b602f
Madhur Pathak <117804227+Madhur1707@users.noreply.github.com>
2025-03-13 23:30:13 +0530

    add transaction server action

--- app/page.js
+++ app/page.js
@@ -54,7 +54,7 @@ export default function Home() {
       </section>
 
         {/* Stats Section */}
-        <section className="py-20 ">
+        <section className="py-10">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
             {statsData.map((stat, index) => (
